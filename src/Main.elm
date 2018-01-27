6fcb0257482c5b1b774d4f110116c74525f3edd4
skht777 <skht@skhtlab.com>
2018-01-27 23:45:10 +0900

    現在時刻表示できた

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,8 @@ module Main exposing (..)
 import Html exposing (program, Html, div, img, text, textarea, button)
 import Html.Attributes exposing (src, value)
 import Html.Events exposing (onClick, onInput)
+import Date exposing (..)
+import Task
 import Styles as Styles
 
 
@@ -21,13 +23,14 @@ type alias Model =
 
 
 type alias Timeline =
-    { index: Index, text : String, isEdit: Bool }
+    { index: Index, text : String, time: Maybe Date, isEdit: Bool }
 
 type alias Index = Int
 
 type Msg
     = EditMsg String
     | Post
+    | PostWithTime Date
     | Delete Index
     | Edit Index
     | EditComp Index
@@ -36,7 +39,7 @@ type Msg
 
 init : ( Model, Cmd Msg )
 init =
-    ( { current = Timeline 0 "" False, timelines = [] }, Cmd.none )
+    ( { current = Timeline 0 "" Nothing False, timelines = [] }, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -48,15 +51,63 @@ update msg model =
             EditMsg msg ->
                 ( { model | current = { timeline | text = msg } }, Cmd.none )
             Post -> 
-                ( { model | current = { timeline | index = timeline.index + 1 }, timelines = timeline :: model.timelines }, Cmd.none )
+                ( model, Task.perform PostWithTime Date.now )
+            PostWithTime now ->
+                let
+                    timelineWithTime = { timeline | time = Just now }
+                    nextTimeline = { timeline | index = timeline.index + 1, text = "" }
+                in
+                    ( { model | current = nextTimeline, timelines = timelineWithTime :: model.timelines }, Cmd.none )
             Delete index ->
-                ( { model | timelines = List.filter (\tl -> index /= tl.index) model.timelines }, Cmd.none )
+                ( { model | timelines = List.filter ( \tl -> index /= tl.index ) model.timelines }, Cmd.none )
             Edit index ->
-                ( { model | timelines = List.map (\tl -> if index == tl.index then { tl | isEdit = True } else tl ) model.timelines }, Cmd.none )
+                ( { model | timelines = List.map ( \tl -> if index == tl.index then { tl | isEdit = True } else tl ) model.timelines }, Cmd.none )
             EditComp index ->
-                ( { model | timelines = List.map (\tl -> if index == tl.index then { tl | isEdit = False } else tl ) model.timelines }, Cmd.none )
+                ( { model | timelines = List.map ( \tl -> if index == tl.index then { tl | isEdit = False } else tl ) model.timelines }, Cmd.none )
             EditChange index msg ->
-                ( { model | timelines = List.map (\tl -> if index == tl.index then { tl | text = msg } else tl ) model.timelines }, Cmd.none )
+                ( { model | timelines = List.map ( \tl -> if index == tl.index then { tl | text = msg } else tl ) model.timelines }, Cmd.none )
+
+
+monthNumber: Date -> Int
+monthNumber date =
+    let
+        month = Date.month date
+    in
+        case month of
+            Jan -> 1
+            Feb -> 2
+            Mar -> 3
+            Apr -> 4
+            May -> 5
+            Jun -> 6
+            Jul -> 7
+            Aug -> 8
+            Sep -> 9
+            Oct -> 10
+            Nov -> 11
+            Dec -> 12
+            
+
+
+formatDate: Date -> String
+formatDate date =
+    let
+        ymd = List.map ( \func -> func date |> toString )
+            [ Date.year
+            , monthNumber
+            , Date.day
+            ]
+        hms = List.map ( \func -> func date |> toString )
+            [ Date.hour
+            , Date.minute
+            , Date.second
+            ]
+    in
+        String.concat 
+            [ String.join "/" ymd
+            , " "
+            , String.join ":" hms
+            ]
 
 
 
@@ -85,7 +136,7 @@ viewEditingTalk tl =
                 [ div [ Styles.posterName ] [ text "とみざわ" ]
                 , textarea [ Styles.editingMessage, onInput ( EditChange tl.index ), value tl.text ] []
                 , div [ Styles.talkFooter ]
-                    [ text "2018/01/27 13:30"
+                    [ text <| Maybe.withDefault "fail" <| Maybe.map formatDate tl.time
                     , div [ Styles.buttons ]
                         [ button [ Styles.editButton, onClick ( EditComp tl.index )  ] [ text "完了" ]
                         , button [ Styles.deleteButton, onClick ( Delete tl.index ) ] [ text "削除" ]
@@ -104,7 +155,7 @@ createTimeline tl =
                 [ div [ Styles.posterName ] [ text "とみざわ" ]
                 , div [ Styles.message ] [ text tl.text ]
                 , div [ Styles.talkFooter ]
-                    [ text "2018/01/27 13:30"
+                    [ text <| Maybe.withDefault "fail" <| Maybe.map formatDate tl.time
                     , div [ Styles.buttons ]
                         [ button [ Styles.editButton, onClick ( Edit tl.index )  ] [ text "編集" ]
                         , button [ Styles.deleteButton, onClick ( Delete tl.index ) ] [ text "削除" ]
